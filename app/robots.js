14b944102e2707a5749bc60533ff87c1e431d953
sethipurse <sethipurse@gmail.com>
2026-06-03 16:23:12 +0530

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,12 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+    ],
+    sitemap: 'https://sethi-purse.vercel.app/sitemap.xml',
+  };
+}
